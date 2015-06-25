89c4398aedd1af7436d4b77355a52789a13fb963
selbekk <kristofer.selbekk@bekk.no>
2015-06-25 21:26:07 +0200

    Add a watch task, since it's documented

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-var gulp = require('gulp');
-var style = require('./gulp/style');
+var config = require('./gulp/config'),
+    gulp = require('gulp'),
+    style = require('./gulp/style');
 
 gulp.task('build:style', style.build);
+
+gulp.task('watch', function() {
+    gulp.watch(config.PATHS.LESS.ALL);
+});
 
 gulp.task('default', ['build:style']);
