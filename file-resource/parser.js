006293dbe10dc82e5c0469ed5b374526ee717407
selbekk <kristofer.selbekk@bekk.no>
2015-06-26 18:15:04 +0200

    Remove title from content

--- file-resource/parser.js
+++ file-resource/parser.js
@@ -3,8 +3,12 @@ var marked = require('marked');
 exports.parse = function(markdown, slug) {
     markdown = markdown.trim();
 
-    var title = markdown.split('\n')[0];
+    var lines = markdown.split('\n');
+
+    var title = lines[0];
     title = title.substring(title.indexOf('# ') + 2);
 
-    return { slug: slug.split('.md')[0], title: title, body: marked(markdown) };
+    var content = lines.slice(1).join('\n');
+
+    return { slug: slug.split('.md')[0], title: title, body: marked(content) };
 };
